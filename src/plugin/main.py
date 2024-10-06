e66bd779ce186bfce9877f7acb15b221bd78dab9
jun6292 <c68254@gmail.com>
2024-10-06 14:23:34 +0000

    [Feat] github actions cloud type 분리 및 github_actions.yaml 작성

--- src/plugin/main.py
+++ src/plugin/main.py
@@ -1,7 +1,10 @@
 from spaceone.inventory.plugin.collector.lib.server import CollectorPluginServer
 
 from typing import Generator
+# from plugin.manager.github_repository.github_manager import GithubManager
+# from plugin.manager.github_actions.github_actions_manager import GithubActionsManager
 from plugin.manager.github_manager import GithubManager
+from plugin.manager.github_actions_manager import GithubActionsManager
 
 app = CollectorPluginServer()
 
@@ -43,7 +46,8 @@ def collector_verify(params: dict) -> None:
 
 
 @app.route('Collector.collect')
-def collector_collect(params: dict) -> dict:
+# def collector_collect(params: dict) -> dict:
+def collector_collect(params: dict) -> Generator[dict, None, None]:
     """ Collect external data
 
     Args:
@@ -114,7 +118,17 @@ def collector_collect(params: dict) -> dict:
     schema = params.get("schema")
 
     github_mgr = GithubManager()
-    return github_mgr.collect_resources(options, secret_data, schema)
+    github_actions_mgr = GithubActionsManager()
+    
+    # GithubManager의 리소스 수집 (리포지토리 관련 데이터)
+    for resource in github_mgr.collect_resources(options, secret_data, schema):
+        yield resource
+
+    # GithubActionsManager의 리소스 수집 (Actions 관련 데이터)
+    for resource in github_actions_mgr.collect_resources(options, secret_data, schema):
+        yield resource
+        
+    # return github_mgr.collect_resources(options, secret_data, schema)
 
 
 @app.route('Job.get_tasks')
